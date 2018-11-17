22e5fdf8e38e13daa4d175ea7eb3422e9bb37f1b
Stollener The <stollener@gmail.com>
2018-11-17 11:49:28 +0900

    Initially create function of decide whether image is hotdog or not using CoreML and InceptionV3

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,14 +7,78 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Error converting CIImage")
+            }
+            
+            detect(image: ciimage)
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
 
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Error loading CoreMLModel")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Failed to load results")
+            }
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                    self.navigationController?.navigationBar.barTintColor = UIColor.green
+                } else {
+                    self.navigationItem.title = "NOT Hotdog!"
+                    self.navigationController?.navigationBar.barTintColor = UIColor.red
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+     
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    
+        present(imagePicker, animated: true, completion: nil)
+    
+    }
+    
 
 }
 
